bbd7466639d460400b92bcbbb658582f063dd8d8
CompSciHigh24 <csh-github@compscihigh.org>
2024-11-11 07:47:58 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// ARRAY SECTION
+
+// Task 1: Create an empty array called readingList
+
+// Task 2: Add 3 books to the readingList array
+
+// Task 3: Update the second book to have "Updated" at the end of its title
+
+// Task 4: Delete the last book from the readingList array
+// Console log readingList
+
+// OBJECTS SECTION
+
+// Task 5: Create an object called bookData with 'title', 'author', and 'pages' properties
+
+// Task 6: Add a property 'read' to the bookData object and set it to false
+
+// Task 7: Update the 'read' property to true
+// Console log the bookData object
+
+// NESTED ARRAYS & OBJECTS SECTION
+// Task 8:  Add an property 'chapters' and set it equal to an array holding "Chapter 1", "Chapter 2", and "Chapter 3"
+
+// Task 9: Add "Chapter 4" the 'chapters' property
+
+// Console log the length of bookData's 'chapters' array to confirm it worked
+
+// Task 10: Update the title of the first chapter in the 'chapters' array to be "Chapter 1: Introduction"
+
+// Console log the bookData object
+
